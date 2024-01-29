5fffa990b31f2a18067ee0892ec66464fc005d42
cornchiplick <ilsunkiss@gmail.com>
2024-01-29 18:05:15 +0900

    initial : 기본 패키지 추가 - 패키지 추가   - json-server   - recoil   - react-query   - react hook form   - axios   - dayjs   - mui - json-server 초기 세팅

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 5555,
+      proxy: {
+        "/mock": {
+          target: env.VITE_SERVER_MOCK,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/mock/, ""),
+          secure: false,
+        },
+      },
+    },
+  };
+});

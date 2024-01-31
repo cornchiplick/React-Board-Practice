249b78e20f0d0a7d482055f7e712aa7b2ccfdac1
cornchiplick <ilsunkiss@gmail.com>
2024-01-31 14:05:42 +0900

    Test: mui 이용하여 테마 변경 해보기

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 import DebugObserver from './recoil/DebugObserver.jsx'
+import { ThemeProvider, createTheme } from '@mui/material'
+import { blue, red } from '@mui/material/colors'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,15 +24,38 @@ const queryClient = new QueryClient({
   }
 });
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      lighter: '#C8FAD6',
+      light: '#5BE49B',
+      main: '#00A76F',
+      dark: '#007867',
+      darker: '#004B50',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      lighter: '#EFD6FF',
+      light: '#C684FF',
+      main: '#8E33FF',
+      dark: '#5119B7',
+      darker: '#27097A',
+      contrastText: '#FFFFFF',
+    }
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RecoilRoot>
       <DebugObserver/>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={true}/>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
       </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
